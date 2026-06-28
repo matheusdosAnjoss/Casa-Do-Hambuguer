fc99bb8d3baca18a133a00b01c40328f1771ad84
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-06-28 20:18:06 -0300

    Criando o header

---
+++ front-end/src/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="bg-[#161410]">
+      <div className="w-full md:w-[737px] p-3 md:p-0 mx-auto flex items-center justify-between">
+        <img src="./logo.png" alt="" />
+        <button className="w-[130px] h-[35px] bg-[#F2DAAC] rounded-[10px]">Entra</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

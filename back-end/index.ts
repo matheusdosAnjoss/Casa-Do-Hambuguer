4f3f4796a06e7cfc067ffef02e96ed35dbbcd649
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-07-10 02:45:40 +0200

    Criando o primeiro endpoint

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
-const app = express()
+const app = express();
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World')
-// })
+app.get("/teste", (req, res) => {
+    res.send("Hello World");
+    console.log("Sevidor conectado");
+});
 
 app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000')
-}) 
+  console.log("Servidor rodando em http://localhost:3000");
+}); // node --watch index.ts

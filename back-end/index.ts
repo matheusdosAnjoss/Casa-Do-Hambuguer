20126088ba37b34516c49823efb41b9e5b5ec928
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-07-10 02:30:59 +0200

    Criando servidor express

---
+++ back-end/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express()
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World')
+// })
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000')
+}) 

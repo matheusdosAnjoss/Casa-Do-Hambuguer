dd5bd0d913e17d0199fab1e326ebab2077cb0f9b
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-08-18 22:00:43 -0300

    Ajustes na interface

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import cors from "cors";
 import { connection } from "./src/db.js";
 import { prisma } from "./src/db.js";
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 connection();
 // console.log(process.env.DATABASE_URL)
-app.post("/login", async (req, res) => {
+app.post("/login", async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
@@ -31,9 +31,41 @@ app.post("/login", async (req, res) => {
     }
 
     res.status(200).json({ message: "Login realizado com sucesso", user });
-    
   } catch (error) {
     return res.status(500).json({ message: "Erro no servidor" });
+  }
+});
+
+app.post("/resgister", async (req: Request, res: Response) => {
+  try {
+    const { name, email, password, cep } = req.body;
+    // ! = verificar se é falso, ou se não envio
+    if (!name || !email || !password || !cep) {
+      res
+        .status(400)
+        .json({ message: "Todas as informações são obrigatorias" });
+      return;
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {email: email},
+    });
+
+    if (user?.email) {
+      res.status(409).json({ message: "E-mail ja cadastrado" });
+      return;
+    }
+
+    const newUser = await prisma.user.create({
+      data: { name: name, email: email, password: password, cep: cep },
+    });
+              
+
+    res.status(201).json(newUser);
+
+  } catch (error) {
+    res.status(500).json({ message: "Erro no servidor" });
+    return;
   }
 });
 
